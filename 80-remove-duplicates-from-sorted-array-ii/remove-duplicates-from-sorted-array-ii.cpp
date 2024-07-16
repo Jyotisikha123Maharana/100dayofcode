afcdad991060df0b9e1575f74704283b19cad5e4
Jyotisikha Maharana <93782353+Jyotisikha123Maharana@users.noreply.github.com>
2024-07-16 19:05:13 +0530

    Time: 3 ms (73.05%) | Memory: 15.1 MB (6.04%) - LeetSync

---
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+       int k=0;
+        map<int, int> mp;
+        for(auto it : nums) {
+            mp[it]++;
+            if(mp[it] <= 2) nums[k++] = it;
+        }
+        return k;
+    }  
+    
+};
