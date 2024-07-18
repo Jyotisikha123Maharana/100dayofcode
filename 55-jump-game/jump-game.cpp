58d23d2c3bcbdf63d260a0c88709cf19356aec08
Jyotisikha Maharana <93782353+Jyotisikha123Maharana@users.noreply.github.com>
2024-07-19 01:20:36 +0530

    Time: 114 ms (18.68%) | Memory: 51.1 MB (26.22%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -14,12 +14,28 @@ public:
        }
       
        return false ;*/
-       int goal = nums.size() - 1 ;
+       /*int goal = nums.size() - 1 ;
        for ( int i = nums.size( )- 2; i >= 0 ; i --){
         if ( i+nums[i] >= goal ){
             goal = i ;
         }
        }
        return goal == 0 ; 
+    }*/
+    //bool canJump(vector<int>& nums) {
+        int maxi=INT_MIN, index=-1;
+        if(nums.size()==1) return true;
+        for(int i=0;i<nums.size()-1;i+=index-i){
+            if(nums[i]==0) return false;
+            if(nums[i]+i>=nums.size()-1) return true;
+            for(int j=i+1;j<=nums[i]+i;j++){
+                if(nums[j]+j>maxi){
+                    maxi=nums[j]+j;
+                    index=j;
+                }
+            }
+            maxi=INT_MIN;
+        }
+        return false;
     }
 };
