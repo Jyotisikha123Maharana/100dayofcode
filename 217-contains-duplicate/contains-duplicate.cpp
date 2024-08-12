1d9633356fc3094246cd783ededfa357e3965e89
Jyotisikha Maharana <93782353+Jyotisikha123Maharana@users.noreply.github.com>
2024-08-13 01:34:26 +0530

    Time: 86 ms (77.54%) | Memory: 73.4 MB (45.25%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        map<int, int> mp ;
+        /*map<int, int> mp ;
         int n = nums.size();
         for ( int i = 0 ; i < n ; i++){
             mp[nums[i]]++;
@@ -12,6 +12,39 @@ public:
             }
             
         }
-        return false;
+        return false;*/
+       /* int n = nums.size();
+      for ( int i = 0 ; i < n ; i++){
+        for ( int j = 0 ; j < n ; j++){
+            if (nums[i]== nums[j]){
+                return true ;
+            }
+        }
+        
+      }
+      return false ;*/
+     /* int n = nums.size();
+for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {  // Increment j instead of i
+        if (i != j && nums[i] == nums[j]) {
+            return true;
+        }
     }
+}
+return false;*/
+
+    unordered_set<int> numSet;
+    
+    for (int num : nums) {
+        if (numSet.find(num) != numSet.end()) {
+            return true; // Duplicate found in the set
+        }
+        numSet.insert(num); // Add unique element to the set
+    }
+    
+    return false; // No duplicate found
+
+}
+
+    
 };
