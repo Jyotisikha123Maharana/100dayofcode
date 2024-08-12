e926eebe43bdc718d1ef35288c48f568ca377e3e
Jyotisikha Maharana <93782353+Jyotisikha123Maharana@users.noreply.github.com>
2024-08-13 01:19:33 +0530

    Time: 142 ms (6.64%) | Memory: 77.2 MB (5.13%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        map<int, int> mp ;
+        int n = nums.size();
+        for ( int i = 0 ; i < n ; i++){
+            mp[nums[i]]++;
+        }
+        for ( auto i : mp){
+            if ( i.second >= 2){
+                return true;
+            }
+            
+        }
+        return false;
+    }
+};
