930cad940bc9983d3acf2ea5f323376960203e0f
Jyotisikha Maharana <93782353+Jyotisikha123Maharana@users.noreply.github.com>
2024-09-19 22:33:48 +0530

    Time: 2 ms (81.15%) | Memory: 7.3 MB (78.42%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,12 +1,29 @@
 class Solution {
 public:
     int fib(int n) {
-        if (n==0){
+        /*if (n==0){
             return 0;
         }
         if (n==1){
             return 1 ;
         }
-        return fib(n-1)+fib(n-2);
+        return fib(n-1)+fib(n-2);*/
+        int current_element = 0;
+        int slast =0;
+        int last = 1 ;
+        if(n==0){
+            return 0 ;
+        }
+        if (n== 1){
+            return 1;
+        }
+        for ( int i = 2 ; i <=n ; i++){
+           current_element = slast + last ;
+            slast = last;
+            last = current_element;
+            
+
+        }
+        return current_element;
     }
 };
