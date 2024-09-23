d6bf81d826c87f78a8a9dce7a3ebecd538e2d4ca
Jyotisikha Maharana <93782353+Jyotisikha123Maharana@users.noreply.github.com>
2024-09-23 21:29:19 +0530

    Time: 78 ms (60.98%) | Memory: 63.9 MB (53.72%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -11,7 +11,7 @@ public:
             }
         }
         return 1;*/
-        int ans = -1 ;
+        /*int ans = -1 ;
         for ( int i = 0 ; i <nums.size(); i++){
             int index = abs(nums[i]);
             if ( nums[index]<0){
@@ -19,6 +19,10 @@ public:
             }
             nums[index]*=-1;
         }
-        return ans ;
+        return ans ;*/
+        while (nums[0]!=nums[nums[0]]){
+            swap(nums[0],nums[nums[0]]);
+        }
+        return nums[0];
     }
 };
